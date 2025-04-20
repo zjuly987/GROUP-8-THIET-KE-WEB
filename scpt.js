51cd601ce60ee0156fda31e6ccdbef91a5f9e5ae
phuc252005 <thie.phuc2502@gmail.com>
2025-04-20 21:16:52 +0700

    Add files via upload

---
+++ scpt.js
@@ -0,0 +1,11 @@
+// Hàm giả lập chuyển đổi ngôn ngữ
+function changeLanguage(lang) {
+  alert(`Chuyển sang ngôn ngữ: ${lang}`);
+  // TODO: Tải nội dung ngôn ngữ từ JSON hoặc cập nhật DOM
+}
+
+            // Hàm giả lập chuyển đổi ngôn ngữ
+            function changeLanguage(lang) {
+              alert(`Chuyển sang ngôn ngữ: ${lang}`);
+              // TODO: Tải nội dung ngôn ngữ từ JSON hoặc cập nhật DOM
+            }
