33f670fc48c45dd8691cc7dd4736daa16c77e99c
Thu Trang <saira87205@gmail.com>
2025-06-08 03:04:36 +0700

    thêm menu vào index thử

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -128,6 +128,16 @@ function revealReviewItems() {
 window.addEventListener('scroll', revealReviewItems);
 window.addEventListener('load', revealReviewItems);
 
+//Js cho nút đóng mở nav
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.getElementById('hamburger-btn');
+    const nav = document.getElementById('main-nav');
+
+    btn.addEventListener('click', () => {
+      nav.classList.toggle('active');
+    });
+  });
+
 // Nút về đầu trang
 window.onscroll = function () {
   const btn = document.getElementById("backToTopBtn");
